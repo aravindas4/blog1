ce111a09b74865d1a49ef19b39a87170e3615a5d
aravindas4 <ash.g.education@gmail.com>
2020-05-31 12:34:29 +0530

    feat(blog-model): Add model for blog - Add model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
