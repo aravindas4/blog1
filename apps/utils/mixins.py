84ae284d58480d6a5652f6397066ba63b0d1daa9
aravindas4 <ash.g.education@gmail.com>
2020-05-31 15:22:39 +0530

    Add project util files

---
+++ apps/utils/mixins.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.response import Response
+
+
+class MultiCreateModelMixin(CreateModelMixin):
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED,
+                        headers=headers)
